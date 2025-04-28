f6dc711bfa84fe3316a8e203fc495b1c7cde1e95
Maniha-dev <nasirrazaptcl@gmail.com>
2025-04-28 00:45:11 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+let start=("Let's Calculate the Average Age")
+console.log(start);
+let tage=0
+let age1=parseInt(prompt("Enter 1 Student age "))
+tage+=age1;
+let age2=parseInt(prompt("Enter 2 Student age "))
+tage+=age2;
+let age3=parseInt(prompt("Enter 3 Student age "))
+tage+=age3;
+let age4=parseInt(prompt("Enter 4 Student age "))
+tage+=age4;
+let age5=parseInt(prompt("Enter 5 Student age "))
+tage+=age5;
+let age6=parseInt(prompt("Enter 6 Student age "))
+tage+=age6;
+let age7=parseInt(prompt("Enter 7 Student age "))
+tage+=age7;
+let age8=parseInt(prompt("Enter 8 Student age "))
+tage+=age8;
+let age9=parseInt(prompt("Enter 9 Student age "))
+tage+=age9;
+let age10=parseInt(prompt("Enter 10 Student age "))
+tage+=age10;
+let avg=tage/10;
+let pro=(alert("Average age is " + avg))
+let display=("Average age is " + avg)
+console.log(display);
+
